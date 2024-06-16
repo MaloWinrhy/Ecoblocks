767832dbbcfba96c4627e6a554ee28a9ca2288f9
MaloWinrhy <malo@arkada.fr>
2024-06-16 13:57:06 +0200

    feat(WEBAPP): :sparkles: Create Newsletter.js

---
+++ ecoblock-webapp/src/components/newsletter/Newsletter.js
@@ -0,0 +1,18 @@
+// src/components/newsletter/Newsletter.js
+
+import React from 'react';
+import './Newsletter.css';
+
+const Newsletter = () => {
+  return (
+    <div className="newsletter">
+      <h2>Subscribe to our Newsletter</h2>
+      <form>
+        <input type="email" placeholder="Enter your email" required />
+        <button type="submit">Subscribe</button>
+      </form>
+    </div>
+  );
+};
+
+export default Newsletter;
