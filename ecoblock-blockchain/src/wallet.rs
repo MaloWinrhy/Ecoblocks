aa593ca6f5610d727c0478ef159014fbd8f77b3b
MaloWinrhy <malo@arkada.fr>
2024-06-19 14:30:24 +0200

    feat(BLOCKCHAIN): :sparkles: Create debit and credit function in the wallet.rs

--- ecoblock-blockchain/src/wallet.rs
+++ ecoblock-blockchain/src/wallet.rs
@@ -12,5 +12,23 @@ impl Wallet {
         }
     }
 
+    pub fn generate_address() -> String {
+        let mut rng = rand::thread_rng();
+        (0..32).map(|_| rng.gen_range(0..10).to_string()).collect()
+    }
 
+    pub fn credit(&mut self, address: &String, amount: u64) {
+        let balance = self.balances.entry(address.clone()).or_insert(0);
+        *balance += amount;
+    }
+
+    pub fn debit(&mut self, address: &String, amount: u64) -> Result<(), String> {
+        let balance = self.balances.entry(address.clone()).or_insert(0);
+        if *balance >= amount {
+            *balance -= amount;
+            Ok(())
+        } else {
+            Err(String::from("Insufficient balance"))
+        }
+    }
 }
