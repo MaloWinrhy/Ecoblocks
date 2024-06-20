fff108acce153d7c9179c2fb68a42c1063ea186b
MaloWinrhy <malo@arkada.fr>
2024-06-20 11:43:26 +0200

    fix(BLOCKCHAIN): :bug: Suppress unused files

--- ecoblock-blockchain/src/consensus.rs
+++
@@ -1,9 +0,0 @@
-use crate::block::Block;
-
-pub struct Consensus;
-
-impl Consensus {
-    pub fn validate_block(block: &Block) -> bool {
-        !block.previous_hashes.is_empty()
-    }
-}
--- ecoblock-blockchain/target/debug/deps/ecoblock_blockchain-2f529f6df904ab3c.d
+++ ecoblock-blockchain/target/debug/deps/ecoblock_blockchain-2f529f6df904ab3c.d
@@ -1,10 +1,9 @@
-/Users/malohenry/development/NFS/Ecoblocks/ecoblock-blockchain/target/debug/deps/libecoblock_blockchain-2f529f6df904ab3c.rmeta: src/main.rs src/db.rs src/block.rs src/tangle.rs src/consensus.rs src/wallet.rs
+/Users/malohenry/development/NFS/Ecoblocks/ecoblock-blockchain/target/debug/deps/libecoblock_blockchain-2f529f6df904ab3c.rmeta: src/main.rs src/db.rs src/block.rs src/tangle.rs src/wallet.rs
 
-/Users/malohenry/development/NFS/Ecoblocks/ecoblock-blockchain/target/debug/deps/ecoblock_blockchain-2f529f6df904ab3c.d: src/main.rs src/db.rs src/block.rs src/tangle.rs src/consensus.rs src/wallet.rs
+/Users/malohenry/development/NFS/Ecoblocks/ecoblock-blockchain/target/debug/deps/ecoblock_blockchain-2f529f6df904ab3c.d: src/main.rs src/db.rs src/block.rs src/tangle.rs src/wallet.rs
 
 src/main.rs:
 src/db.rs:
 src/block.rs:
 src/tangle.rs:
-src/consensus.rs:
 src/wallet.rs:
