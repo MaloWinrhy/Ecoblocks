037779ca138be463b145940c8e625fb3f87c1316
MaloWinrhy <malo@arkada.fr>
2024-06-21 13:20:13 +0200

    feat(CMS): :sparkles: Create postList.js

---
+++ ecoblock-cms/src/posts/postList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+
+const PostList = (props) => (
+  <List {...props}>
+    <Datagrid>
+      <TextField source="id" />
+      <TextField source="title" />
+      <ReferenceField source="userId" reference="users">
+        <TextField source="name" />
+      </ReferenceField>
+      <EditButton />
+    </Datagrid>
+  </List>
+);
+
+export default PostList;
