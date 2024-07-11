a2ac0f8f8666e7c9632247c192e9e27962a2391f
MaloWinrhy <malo@arkada.fr>
2024-07-11 14:10:13 +0200

    feat(WEBAPP): :sparkles: Limite DevBlog list post to maxLength 150

--- ecoblock-webapp/src/components/blog/BlogPost.js
+++ ecoblock-webapp/src/components/blog/BlogPost.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import './BlogPost.css';
 import { format, parseISO } from 'date-fns';
+import { Link } from 'react-router-dom';
+import { truncateText } from '../../utils';
 
 const BlogPost = ({ post }) => {
   const formattedDate = post.created_at ? format(parseISO(post.created_at), 'MMMM d, yyyy') : 'Invalid Date';
+  const maxLength = 150;
 
   return (
     <div className="blog-post">
       <div className="blog-post-content">
         <div className="blog-post-date">{formattedDate}</div>
-        <h2>{post.title}</h2>
-        <ReactMarkdown>{post.content}</ReactMarkdown>
+        <h2><Link to={`/post/${post.id}`}>{post.title}</Link></h2>
+        <ReactMarkdown>{truncateText(post.content, maxLength)}</ReactMarkdown>
         <div className="blog-post-tags">
           {post.tags.map((tag, index) => (
             <span key={index} className="blog-post-tag">{tag}</span>
